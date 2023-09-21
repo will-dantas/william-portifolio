b37be22de3fee1580e881b212486c61bc77254f2
will-dantas <wmdantas@gmail.com>
2023-09-21 10:09:36 -0300

    Adjust next image

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -37,12 +37,14 @@ export const Content = styled.div`
 `;
 
 export const ContentImage = styled.div`
-
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 4rem;
+  }
 `;
 
 export const ImageWrapper = styled(Image)`
   object-fit: cover;
-  width: 100%;
-  height: 4rem;
 `;
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import { BurguerMenu } from '../BurguerMenu/BurguerMenu';
 import { Container, Content, ContentImage, ImageWrapper } from './Header.styles';
-import logo from "../../../public/logo-wd.png"
+import Logo from "../../../public/logo-wd.png"
+import Image from 'next/image';
 
 export const Header = () => {
   return (
     <Container>
       <Content>
         <ContentImage>
-          <ImageWrapper src={logo} alt="Logo William Dantas" />
+          <ImageWrapper height={35} width={500} src={Logo} alt="Logo William Dantas" />
         </ContentImage>
         <BurguerMenu />
       </Content>
