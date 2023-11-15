de353de77ac3f7bb1b48cce2f4b075d1c5ee1961
will-dantas <wmdantas2@gmail.com>
2023-11-15 16:30:23 -0300

    feat: Adjust header +

--- src/components/SideBar/SideBar.styles.ts
+++ src/components/SideBar/SideBar.styles.ts
@@ -19,8 +19,8 @@ export const Ul = styled.ul<SideBarProps>`
     border-radius: 0.5rem;
     padding: 1.5rem;
     position: fixed;
-    top: 6rem;
-    left: 0;
+    top: 110px;
+    left: 20px;
     width: 16rem;
     transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
     transition: transform 0.3s ease-in-out;
